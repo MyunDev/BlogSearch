38487c7dbbecaeb894eb21ff6fe5644fef22ce7a
김현기 <hgk1221@shinsegae.com>
2023-03-22 01:20:12 +0900

    [Add] 초기 세팅 데이터 추가

--- search/src/main/resources/data.sql
+++ search/src/main/resources/data.sql
@@ -2,3 +2,18 @@ insert
 into keyword
     (edit_dtm, count, keyword, reg_dtm)
 values (null, 3, '카카오', '2023-03-21 21:00:00');
+
+insert
+into keyword
+(edit_dtm, count, keyword, reg_dtm)
+values (null, 10, '카카오뱅크', '2023-03-21 21:00:00');
+
+insert
+into keyword
+(edit_dtm, count, keyword, reg_dtm)
+values (null, 4, '코딩', '2023-03-21 21:00:00');
+
+insert
+into keyword
+(edit_dtm, count, keyword, reg_dtm)
+values (null, 8, '스프링부트', '2023-03-21 21:00:00');
